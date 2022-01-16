aba5e9f0296bbbb9cdb9bb3775ef29dc65dd4f9a
stasserebrennikov59@gmail.com <stasserebrennikov59@gmail.com>
2022-01-16 22:28:02 +0300

    Добавьте файлы проекта.

---
+++ Lab_12/Circle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_12
+{
+
+    static class Circle
+    {
+        static public double GetLeghtCircle(double R)
+        {
+            return 2 * Math.PI * R;
+        }
+        static public double GetSquare(double R)
+        {
+            return Math.PI * R * R;
+        }
+        static public string Centr( double x, double y, double R)
+        {
+            double Centr = Math.Sqrt(Math.Pow(0 - x, 2) + Math.Pow(0 - y, 2));
+
+            if (Centr < R)
+                return "{Точка находится в кругу}";
+            else
+                return "Точка вне круга";
+                
+
+        }
+
+    }
+}
---
+++ Lab_12/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double R = Convert.ToDouble(Console.ReadLine());
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            double leghtcircle = Circle.GetLeghtCircle(R);
+            double square = Circle.GetSquare(R);
+            string centr = Circle.Centr(x, y, R);
+            Console.WriteLine($"{leghtcircle}\n {square}\n {centr}");
+            Console.ReadKey();
+
+        }
+    }
+}
